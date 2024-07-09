0912161d44f7e1a9146e4c8334d5ffa73e6be6d5
Marc Keeling <marc.keeling@pointclickcare.com>
2024-07-09 14:39:34 -0600

    custom damin for recipe model

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from .models import Recipe
 
-admin.site.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    """
+    Custom admin for the Recipe model to enhance admin interface interaction.
+
+    Attributes:
+        list_display (tuple): Fields displayed in the list view.
+        list_filter (tuple): Fields used for filtering in the admin list.
+        search_fields (tuple): Fields searchable in the admin interface.
+    """
+    list_display = ('name', 'author', 'created_at')
+    list_filter = ('created_at', 'author')
+    search_fields = ('name', 'description')
+
+admin.site.register(Recipe, RecipeAdmin)
