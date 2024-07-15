6c6027478759063d88ff42750d0b5f72100afc95
Marc Keeling IV <marckeelingiv@gmail.com>
2024-07-14 22:05:41 -0600

    added comments for clarity

--- accounts/models.py
+++ accounts/models.py
@@ -2,9 +2,20 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 class CustomUserManager(BaseUserManager):
+    """
+    Custom manager for CustomUser model.
+    """
     def create_user(self, username, email, password, first_name, **extra_fields):
+        """
+        Create and save a regular user with the given username, email, password, and first name.
+        """
         if not username:
             raise ValueError('The given username must be set')
+        if not email:
+            raise ValueError('The given email must be set')
+        if not first_name:
+            raise ValueError('The given first name must be set')
+        
         email = self.normalize_email(email)
         user = self.model(username=username, email=email, first_name=first_name)
         user.set_password(password)
@@ -12,6 +23,9 @@ class CustomUserManager(BaseUserManager):
         return user
     
     def create_superuser(self, username, email, password, first_name, last_name):
+        """
+        Create and save a superuser with the given username, email, password, first name, and last name.
+        """
         user = self.create_user(
             email=email,
             username=username,
@@ -25,8 +39,12 @@ class CustomUserManager(BaseUserManager):
         return user
 
 class CustomUser(AbstractUser):
+    """
+    Custom user model extending Django's AbstractUser with an additional is_verify field.
+    """
     is_verify = models.BooleanField(default=False)
     objects = CustomUserManager()
+    
     groups = models.ManyToManyField(
         'auth.Group',
         related_name='custom_user_groups',
